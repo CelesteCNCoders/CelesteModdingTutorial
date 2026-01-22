90e340cb578c9d868cd338d96ac9de3c68f12030
ClearZer0 <534310154@qq.com>
2026-01-22 18:35:57 +0800

    更新测试地图

--- docs/code_modding/src/CelesteModTutorial/Source/CelesteModTutorialModule.cs
+++ docs/code_modding/src/CelesteModTutorial/Source/CelesteModTutorialModule.cs
@@ -1,21 +1,20 @@
-using Celeste.Mod.CelesteModTutorial.Utils;
-
 namespace Celeste.Mod.CelesteModTutorial;
 
 public sealed class CelesteModTutorialModule : EverestModule
 {
     public static CelesteModTutorialModule Instance { get; private set; }
 
+    public override Type SettingsType => typeof(CelesteModTutorialSettings);
+    public static CelesteModTutorialSettings Settings => (CelesteModTutorialSettings)Instance._Settings!;
+
+
     public override void Load()
     {
         Instance = this;
         Logger.Log(LogLevel.Info, "CelesteModTutorial", "Hello, Celeste!");
-
-        AttributeHelper.InvokeAllMethods<LoadAttribute>();
     }
 
     public override void Unload()
     {
-        AttributeHelper.InvokeAllMethods<UnLoadAttribute>();
     }
 }
---
+++ docs/code_modding/src/CelesteModTutorial/Source/CelesteModTutorialSettings.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.CelesteModTutorial;
+
+public class CelesteModTutorialSettings : EverestModuleSettings
+{
+    [DefaultButtonBinding(0, Keys.None)]
+    public ButtonBinding SampleBinding { get; set; }
+}
